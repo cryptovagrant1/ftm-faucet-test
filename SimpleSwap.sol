572b2626e240794e9b5f428af56fd1c2a652219c
cryptovagrant1 <cryptovagrant@pm.me>
2021-06-27 20:03:56 -0400

    1 file modified

--- SimpleSwap.sol
+++ SimpleSwap.sol
@@ -1,32 +1,32 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.7.1;
 
 import "https://github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/interfaces/IUniswapV2Router02.sol";
 
 contract UniswapExample {
-  address internal constant FLOWERSWAP_ROUTER_ADDRESS = 0x57f077e307ab57c5448f2df1d7b59ce07a097cab ;
+  address internal constant FLOWERSWAP_ROUTER_ADDRESS = 0x57F077E307Ab57C5448F2DF1D7B59ce07a097cAb ;
 
   IUniswapV2Router02 public flowerswapRouter;
-  address private multiDaiKovan = 0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa;
-  address private Btc = 0x2647ab2f1f5d90eea784eeb2c4ececcea3bb0baf;
+  address private Btc = 0x2647aB2f1F5d90eeA784eeB2C4EcECcea3bB0BaF;
 
   constructor() {
     flowerswapRouter = IUniswapV2Router02(FLOWERSWAP_ROUTER_ADDRESS);
   }
 
-  function convertEthToBtc(uint btcAmount) public payable {
+  function convertFtmToBtc(uint btcAmount) public payable {
     uint deadline = block.timestamp + 15; // using 'now' for convenience, for mainnet pass deadline from frontend!
-    flowerswapRouter.swapETHForExactTokens{ value: msg.value }(btcAmount, getPathForETHtoBTC(), address(this), deadline);
+    flowerswapRouter.swapETHForExactTokens{ value: msg.value }(btcAmount, getPathForFTMtoBTC(), address(this), deadline);
     
     // refund leftover ETH to user
     (bool success,) = msg.sender.call{ value: address(this).balance }("");
     require(success, "refund failed");
   }
   
-  function getEstimatedETHforBTC(uint btcAmount) public view returns (uint[] memory) {
-    return flowerswapRouter.getAmountsIn(btcAmount, getPathForETHtoBTC());
+  function getEstimatedFTMforBTC(uint btcAmount) public view returns (uint[] memory) {
+    return flowerswapRouter.getAmountsIn(btcAmount, getPathForFTMtoBTC());
   }
 
-  function getPathForETHtoBTC() private view returns (address[] memory) {
+  function getPathForFTMtoBTC() private view returns (address[] memory) {
     address[] memory path = new address[](2);
     path[0] = flowerswapRouter.WETH();
     path[1] = Btc;
@@ -34,6 +34,6 @@ contract UniswapExample {
     return path;
   }
   
-  // important to receive ETH
+  // important to receive FTM
   receive() payable external {}
 }
